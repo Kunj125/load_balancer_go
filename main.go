1fdf2d935e5132746762b4ecae199fd54e9cb2fa
Kunj Dhola <kunjdhola8a@gmail.com>
2025-09-01 17:29:39 -0600

    Server status check functions

--- main.go
+++ main.go
@@ -4,7 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
 )
+
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+func (b *Backend) IsAlive() bool {
+	b.mux.RLock()
+	defer b.mux.RUnlock()
+	return b.Alive
+}
 
 func main() {
 	port := 8080
