ce3405a7308c27ee3315452e3b2db95dd95807b5
Kunj Dhola <kunjdhola8a@gmail.com>
2025-09-02 17:32:22 -0600

    Add serverpool with round robin selection

--- main.go
+++ main.go
@@ -28,6 +28,35 @@ func (b *Backend) IsAlive() bool {
 	return b.Alive
 }
 
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+func (s *ServerPool) AddBackend(b *Backend) {
+	s.backends = append(s.backends, b)
+}
+
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, 1) % uint64(len(s.backends)))
+}
+
+func (s *ServerPool) GetNextPeer() *Backend {
+	next := s.NextIndex()
+	length := len(s.backends)
+
+	for i := 0; i < length; i++ {
+		idx := (next + i) % length
+		if s.backends[idx].IsAlive() {
+			if i != 0 {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
+}
+
 func main() {
 	port := 8080
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
